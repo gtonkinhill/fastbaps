1d190f50cddd1319f11bce8680791750e5444bf1
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2018-07-24 13:41:30 +0100

    updated grid search to use R optimise and have a much broader initial range

--- R/optimise_prior.R
+++ R/optimise_prior.R
@@ -6,9 +6,9 @@
 #'
 #'
 #' @param sparse.data a sparse SNP data object returned from import_fasta_sparse_nt
-#' @param grid.vals the grid values of the hyperparameter to optimise over (default=c(0:20/5))
+#' @param grid.interval the upper and lower bound for the hyperparameter to optimise over (default=c(5e-4, 10))
 #' @param type one of "flat" or "hc" indicating a flat prior or the approach of Heller et al.
-#' @param n.cores number of cores to use
+#' @param n.cores number of cores to use (currently not implemented)
 #'
 #' @return a sparse.data object with the prior optimised via grid search
 #'
@@ -18,17 +18,15 @@
 #' sparse.data <- optimise_prior(sparse.data)
 #'
 #' @export
-optimise_prior <- function(sparse.data, grid.vals=c(1:100/20), type = "flat", n.cores=1){
+optimise_prior <- function(sparse.data, grid.interval=c(5e-4, 10), type = "flat", n.cores=1){
 
   # Check inputs
   if(!is.list(sparse.data)) stop("Invalid value for sparse.data! Did you use the import_fasta_sparse_nt function?")
   if(!(class(sparse.data$snp.matrix)=="dgCMatrix")) stop("Invalid value for sparse.data! Did you use the import_fasta_sparse_nt function?")
   if(!is.numeric(sparse.data$consensus)) stop("Invalid value for sparse.data! Did you use the import_fasta_sparse_nt function?")
   if(!is.matrix(sparse.data$prior)) stop("Invalid value for sparse.data! Did you use the import_fasta_sparse_nt function?")
-  if(!(type %in% c("flat", "hc", "combined"))) stop("Invalid value for type. Must be one of 'flat' or 'hc'")
-  if(!all(grid.vals>0)) stop("grid values must greater than 0")
-
-  MIN_RES <- 1e-3
+  if(!(type %in% c("flat", "hc"))) stop("Invalid value for type. Must be one of 'flat' or 'hc'")
+  if(!all(grid.interval>0)) stop("grid values must greater than 0")
 
   #create hclust object
   h <- get_hclust(sparse.data, TRUE)
@@ -46,61 +44,20 @@ optimise_prior <- function(sparse.data, grid.vals=c(1:100/20), type = "flat", n.
     initial.prior <- ceiling(initial.prior*1000)/1000
     sparse.data$prior <- initial.prior
 
-    ptrees <- unlist(parallel::mclapply(grid.vals, function(cc){
-      sparse.data$prior <- initial.prior * cc
-      sparse.data$prior[sparse.data$prior<MIN_RES] <- MIN_RES
-      llks <- fastbaps:::tree_llk(sparse.data, h$merge)
-      return(llks$ptree[length(llks$ptree)])
-    }, mc.cores = n.cores))
-
-    if(!all(ptrees<0)) stop("Error: tree probabilities exceed 1!")
-
-    bfs <- ptrees[[1]] - ptrees
-
-    cc <- grid.vals[which.min(bfs)]
-    llk.max <- ptrees[which.max(ptrees)]
-    sparse.data$prior <- initial.prior*cc
-  } else if (type=="combined"){
-    #initialise prior
-    initial.prior <- matrix(c(rep(ncol(sparse.data$snp.matrix), nrow(sparse.data$snp.matrix)),
-                              rowSums(sparse.data$snp.matrix==1),
-                              rowSums(sparse.data$snp.matrix==2),
-                              rowSums(sparse.data$snp.matrix==3),
-                              rowSums(sparse.data$snp.matrix==4)), nrow = 5, byrow = TRUE)
-    weight <- colSums(initial.prior)/ncol(sparse.data$snp.matrix)
-    initial.prior[1,] <- initial.prior[1,] - colSums(initial.prior[2:5,])
-    # initial.prior <- initial.prior+1
-    initial.prior <- (initial.prior>0)*1
-    initial.prior <- t(t(initial.prior)/colSums(initial.prior))
-    initial.prior <- t(t(initial.prior)*weight)
-    initial.prior <- ceiling(initial.prior*1000)/1000
-    sparse.data$prior <- initial.prior
-
-    ptrees <- unlist(parallel::mclapply(grid.vals, function(cc){
-      sparse.data$prior <- initial.prior * cc
-      sparse.data$prior[sparse.data$prior<MIN_RES] <- MIN_RES
-      llks <- fastbaps:::tree_llk(sparse.data, h$merge)
-      return(llks$ptree[length(llks$ptree)])
-    }, mc.cores = n.cores))
-
-    bfs <- ptrees[[1]] - ptrees
-
-    cc <- grid.vals[which.min(bfs)]
-    sparse.data$prior <- initial.prior * cc
   } else {
-    initial.prior <- matrix(MIN_RES, nrow = nrow(sparse.data$prior), ncol = ncol(sparse.data$prior))
+    initial.prior <- matrix(1, nrow = nrow(sparse.data$prior), ncol = ncol(sparse.data$prior))
     sparse.data$prior <- initial.prior
+  }
 
-    ptrees <- unlist(parallel::mclapply(grid.vals, function(cc){
-      sparse.data$prior <- initial.prior + cc
-      llks <- fastbaps:::tree_llk(sparse.data, h$merge)
-      return(llks$ptree[length(llks$ptree)])
-    }, mc.cores = n.cores))
+  opt <- optimise(calc_prior_prob, grid.interval, sparse.data, initial.prior, h, maximum = TRUE, tol=1e-3)
+  cc <- round(opt$maximum, digits = 3)
 
-    bfs <- ptrees[[1]] - ptrees
+  sparse.data$prior <- initial.prior*cc
+  sparse.data$prior[sparse.data$prior<1e-3] <- 1e-3
 
-    cc <- grid.vals[which.min(bfs)]
-    sparse.data$prior <- initial.prior+cc
+  ##Check hyperparameter is not too close to grid edges
+  if (any(abs(grid.interval-cc)<5e-3)){
+    warning("Inferred hyperparameter is very close to interval boundries! Consider changing the interval.")
   }
 
   print(paste("Optimised hyperparameter:", cc))
@@ -108,4 +65,13 @@ optimise_prior <- function(sparse.data, grid.vals=c(1:100/20), type = "flat", n.
   sparse.data$prior.type = "optimised"
 
   return(sparse.data)
+}
+
+calc_prior_prob <- function(cc, temp.sparse.data, temp.initial.prior, temp.h){
+  cc <- round(cc, digits = 3) #as otherwise we run into issue with rounding in the llk calculation due to the way lgamma is calculated
+  temp.sparse.data$prior <- temp.initial.prior * cc
+  temp.sparse.data$prior[temp.sparse.data$prior<1e-3] <- 1e-3
+  llks <- fastbaps:::tree_llk(temp.sparse.data, temp.h$merge)
+  prob.tree <- llks$ptree[length(llks$ptree)]
+  return(prob.tree)
 }
